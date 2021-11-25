5a52777954cd57db06d884cf232b82f4592daf97
Burak Yilmaz <58527608+ag3silaus@users.noreply.github.com>
2021-11-25 14:34:04 +0100

    finished

---
+++ script.js
@@ -0,0 +1,99 @@
+var models = [
+    {
+        name : 'BMW 420i',
+        image : 'img/bmw.jpg',
+        link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
+    },
+    {
+        name : 'Mazda CX-3',
+        image : 'img/mazda.jpg',
+        link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
+    },
+    {
+        name : 'Volvo S60',
+        image : 'img/volvo.jpg',
+        link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
+    },
+    {
+        name : 'Skoda Superb',
+        image : 'img/skoda.jpg',
+        link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
+    },
+    {
+        name : 'Honda Civic',
+        image : 'img/honda.jpg',
+        link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'
+    },
+    
+];
+
+var index = 0;
+var slaytCount = models.length;
+var interval;
+
+var settings = {
+    duration : '2000',
+    random : true
+}
+
+init(settings);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
+    index--;
+    showSlide(index);
+});
+
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
+    index++;
+    showSlide(index);
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+});
+
+function init(settings){
+    
+    var prev;
+    interval = setInterval(function(){
+        if(settings.random){
+            do{     
+            index = Math.floor(Math.random() * slaytCount);
+            }while(index === prev)
+            prev = index;
+        }
+        else{
+            if(slaytCount === index ){
+                index = 0;
+            }
+            showSlide(index);
+            index++;
+        }
+        showSlide(index);
+    },settings.duration)
+}
+
+function showSlide(i){
+    index = i;
+    if(i<0){
+        index = slaytCount - 1;
+    }
+    if(i >= slaytCount){
+        index = 0;
+    }
+
+    document.querySelector('.card-title').textContent = models[index].name;
+    document.querySelector('.card-img-top').setAttribute('src', models[index].image);
+    document.querySelector('.card-link').setAttribute('href',models[index].link);
+    
+}
+
+
